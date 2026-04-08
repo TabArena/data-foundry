3374c2280c62c66fca3910b550979b2ba4d2c52c
LennartPurucker <lennart@priorlabs.ai>
2026-04-08 21:02:01 +0000

    add: lung_cancer_epithelial_genexp

--- datasets/beyond_iid/_tmp_state_paths.py
+++ datasets/beyond_iid/_tmp_state_paths.py
@@ -117,6 +117,7 @@ NEW_IID = [
     "ovarian_cancer_prediction/019d6e94-c974-7a4a-9159-334be748c176",
     "prostate_cancer_detection/019d6eb1-3a29-7224-b0b4-c40eb7c82064",
     "pca_genexp_prediction/019d6ec0-f5e5-76fd-afb3-b16610b309ba",
+    "lung_cancer_epithelial_genexp/019d6ee6-0868-7acf-a7fd-34e82d7809c3",
 ]
 
 
